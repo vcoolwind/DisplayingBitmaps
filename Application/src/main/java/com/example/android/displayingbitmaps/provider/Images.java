0cf50c8a05300ba123e34d01116e9073d491afa9
vcoolwind <vcoolwind@163.com>
2016-11-11 16:46:04 +0800

    first

---
+++ Application/src/main/java/com/example/android/displayingbitmaps/provider/Images.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.displayingbitmaps.provider;
+
+/**
+ * Some simple test data to use for this sample app.
+ */
+public class Images {
+
+    /**
+     * This are PicasaWeb URLs and could potentially change. Ideally the PicasaWeb API should be
+     * used to fetch the URLs.
+     * <p>
+     * Credit to Romain Guy for the photos:
+     * http://www.curious-creature.org/
+     * https://plus.google.com/109538161516040592207/about
+     * http://www.flickr.com/photos/romainguy
+     */
+    public final static String[] imageUrls = new String[]{
+            "http://pic22.nipic.com/20120724/2786001_155107381000_2.jpg",
+            "http://pic.58pic.com/58pic/11/36/89/80N58PICMua.jpg",
+            "http://pic19.nipic.com/20120322/6029124_175031240134_2.jpg",
+            "http://pic2.ooopic.com/11/26/77/80b1OOOPIC24.jpg",
+            "http://img10.3lian.com/sc6/show02/42/17/17.jpg",
+            "http://img10.3lian.com/sc6/show02/42/17/19.jpg",
+            "http://www.bz55.com/uploads/allimg/100709/10145GC9-0.jpg",
+            "http://img.sj33.cn/uploads/allimg/201001/20100128144013185.jpg",
+            "http://img3.imgtn.bdimg.com/it/u=1340608177,2469166881&fm=21&gp=0.jpg",
+            "http://pic45.nipic.com/20140730/12005913_100105395000_2.jpg",
+            "http://pic42.nipic.com/20140623/2734391_144024450000_2.jpg",
+            "http://img0.imgtn.bdimg.com/it/u=42595042,1478714033&fm=21&gp=0.jpg",
+            "http://s4.sinaimg.cn/middle/5d6fb672xa3ccc7f486c3&690"
+    };
+    public final static String[] imageThumbUrls = new String[]{
+            "http://img0.imgtn.bdimg.com/it/u=2556212681,497720804&fm=21&gp=0.jpg",
+            "http://img2.imgtn.bdimg.com/it/u=2748968309,3031688374&fm=21&gp=0.jpg",
+            "http://img4.imgtn.bdimg.com/it/u=295910500,1868075674&fm=21&gp=0.jpg",
+            "http://img2.imgtn.bdimg.com/it/u=1488409316,3812545622&fm=21&gp=0.jpg",
+            "http://img2.imgtn.bdimg.com/it/u=4161664902,1430141481&fm=21&gp=0.jpg",
+            "http://img0.imgtn.bdimg.com/it/u=2107247791,568353654&fm=21&gp=0.jpg",
+            "http://img0.imgtn.bdimg.com/it/u=3302988591,335368309&fm=21&gp=0.jpg",
+            "http://img2.imgtn.bdimg.com/it/u=3930001885,1313517093&fm=21&gp=0.jpg",
+            "http://img3.imgtn.bdimg.com/it/u=1340608177,2469166881&fm=21&gp=0.jpg",
+            "http://img3.imgtn.bdimg.com/it/u=3137719145,221449723&fm=21&gp=0.jpg",
+            "http://img1.imgtn.bdimg.com/it/u=2583876790,2878217877&fm=21&gp=0.jpg",
+            "http://img0.imgtn.bdimg.com/it/u=42595042,1478714033&fm=21&gp=0.jpg",
+            "http://img3.imgtn.bdimg.com/it/u=2904012867,2379928432&fm=21&gp=0.jpg"
+    };
+
+}
---
+++ Application/src/main/java/com/example/android/displayingbitmaps/provider/Images2.java
@@ -0,0 +1,25 @@
+package com.example.android.displayingbitmaps.provider;
+
+/**
+ * Created by vcoolwind on 2016/11/10.
+ */
+
+public class Images2 {
+    public final static String[] imageUrls = new String[]{
+            "http://pic22.nipic.com/20120724/2786001_155107381000_2.jpg",
+            "http://pic.58pic.com/58pic/11/36/89/80N58PICMua.jpg",
+            "http://pic19.nipic.com/20120322/6029124_175031240134_2.jpg",
+            "http://pic2.ooopic.com/11/26/77/80b1OOOPIC24.jpg",
+            "http://img10.3lian.com/sc6/show02/42/17/17.jpg",
+            "http://img10.3lian.com/sc6/show02/42/17/19.jpg"
+    };
+    public final static String[] imageThumbUrls = new String[]{
+            "http://img0.imgtn.bdimg.com/it/u=2556212681,497720804&fm=21&gp=0.jpg",
+            "http://img2.imgtn.bdimg.com/it/u=2748968309,3031688374&fm=21&gp=0.jpg",
+            "http://img4.imgtn.bdimg.com/it/u=295910500,1868075674&fm=21&gp=0.jpg",
+            "http://img2.imgtn.bdimg.com/it/u=1488409316,3812545622&fm=21&gp=0.jpg",
+            "http://img2.imgtn.bdimg.com/it/u=4161664902,1430141481&fm=21&gp=0.jpg",
+            "http://img0.imgtn.bdimg.com/it/u=2107247791,568353654&fm=21&gp=0.jpg"
+    };
+
+}
